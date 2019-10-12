dbf0345279c684e9912916f211250581e287a700
João Paulo <joaopauloamesco@gmail.com>
2019-10-12 16:29:27 -0300

    Updates

---
+++ precache-manifest.07637bcca2d887fc218566f618ae8e05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2d2bfef45ee7edc979ad382000ef79",
+    "url": "/dovavision/index.html"
+  },
+  {
+    "revision": "d088000f125b13112b89",
+    "url": "/dovavision/static/css/2.ff9ea1f9.chunk.css"
+  },
+  {
+    "revision": "93f54d1791a216bcd683",
+    "url": "/dovavision/static/css/main.9fdca8b7.chunk.css"
+  },
+  {
+    "revision": "d088000f125b13112b89",
+    "url": "/dovavision/static/js/2.ae4d6612.chunk.js"
+  },
+  {
+    "revision": "93f54d1791a216bcd683",
+    "url": "/dovavision/static/js/main.8386506b.chunk.js"
+  },
+  {
+    "revision": "7c9b0260c1753d76cab1",
+    "url": "/dovavision/static/js/runtime~main.2e84e063.js"
+  },
+  {
+    "revision": "d6cfc46a27ff5041f6b7b86f05da7aeb",
+    "url": "/dovavision/static/media/new-york-1.d6cfc46a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30bde49c9cb03b3d397cd7cce4b55a9e.js"
+  "/dovavision/precache-manifest.07637bcca2d887fc218566f618ae8e05.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dovavision/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
