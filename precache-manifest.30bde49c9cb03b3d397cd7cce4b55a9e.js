0ee71a5fcfecc8933d7ee4486abcca5fe9c605fc
João Paulo <joaopauloamesco@gmail.com>
2019-10-12 16:23:59 -0300

    Updates

---
+++ precache-manifest.30bde49c9cb03b3d397cd7cce4b55a9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1f66d9038db68b088327d97262ca9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96cf74dfbfa705eefdfa",
+    "url": "/static/css/2.ff9ea1f9.chunk.css"
+  },
+  {
+    "revision": "4d73c38a8b7dcb449f84",
+    "url": "/static/css/main.0f4af092.chunk.css"
+  },
+  {
+    "revision": "96cf74dfbfa705eefdfa",
+    "url": "/static/js/2.0b567009.chunk.js"
+  },
+  {
+    "revision": "4d73c38a8b7dcb449f84",
+    "url": "/static/js/main.7dbb6049.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d6cfc46a27ff5041f6b7b86f05da7aeb",
+    "url": "/static/media/new-york-1.d6cfc46a.jpg"
+  }
+]);
